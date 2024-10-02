ec91423e0f09888d2631f4238a8fd7a159d3f84f
Diego Nelson <diegonelson@3lineas.com>
2024-10-01 23:07:52 -0300

    change scrool smooth to hrml tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body
-        className={`${ubuntu.variable} font-sans antialiased scroll-smooth`}
+        className={`${ubuntu.variable} font-sans antialiased`}
       >
         <ThemeProvider
           attribute="class"
