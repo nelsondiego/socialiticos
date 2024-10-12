d5d567c4430825b744d3e31c2b84fc01720dca0a
Diego Nelson <diegonelson@3lineas.com>
2024-10-11 21:58:31 -0300

    delete description mock from auth pages

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,9 +2,6 @@ import Link from "next/link";
 
 import SignUpForm from "./SignUpForm";
 
-export const description =
-  "A login page with two columns. The first column has the login form with email and password. There's a Forgot your passwork link and a link to sign up if you do not have an account. The second column has a cover image.";
-
 export default function SignIn() {
   return (
     <div className="mx-auto grid w-[350px] gap-6">
