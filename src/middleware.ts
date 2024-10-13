534a67a379c6b467ebf6538affbcbde7a912598d
Diego Nelson <diegonelson@3lineas.com>
2024-10-13 12:26:20 -0300

    fixed middleware for static files

--- src/middleware.ts
+++ src/middleware.ts
@@ -29,5 +29,10 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-}
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|avif|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
